a39a75032414616c837d3eefa2b7e21ddc3ef3c1
vipisanan <vipisanan1@gmail.com>
2020-11-07 17:08:03 +0530

    structured

---
+++ src/pages/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            path: '/',
+        };
+    }
+
+    render() {
+        return (
+            <div>vv</div>
+        );
+    }
+}
+export default App;
